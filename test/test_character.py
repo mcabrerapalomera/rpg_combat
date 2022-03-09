a10b75387d7d88e974c7e09633a8f1dd916939f8
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-09 10:09:46 -0500

    Failing test to check if character dies

--- test/test_character.py
+++ test/test_character.py
@@ -2,17 +2,23 @@ from rpg_combat.character import Character
 
 
 class TestCharacter:
+    def setup(self):
+        self.knight = Character()
+        self.dragon = Character()
+
     def test_init(self):
-        character = Character()
-        assert character.health == 1000
-        assert character.level == 1
-        assert character.alive
+        assert self.knight.health == 1000
+        assert self.knight.level == 1
+        assert self.knight.alive
 
     # Characters can Deal Damage to Characters.
     #    - Damage is subtracted from Health
-
     def test_character_gets_damaged(self):
-        knight = Character()
-        dragon = Character()
-        knight.damage(dragon, 2)
-        assert dragon.health == 1000 - 2
+        self.knight.damage(self.dragon, 2)
+        assert self.dragon.health == 1000 - 2
+
+    #    - When damage received exceeds current Health, Health becomes 0 and the character dies
+    def test_character_dies(self):
+        self.knight.damage(self.dragon, 1000)
+        assert self.dragon.health == 0
+        assert not self.dragon.alive
