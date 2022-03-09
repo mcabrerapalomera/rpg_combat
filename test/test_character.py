17f9a4ce12cb05100843659abad82fa6f5a1334c
mcabrerapalomera <patricia_cabrera-palomera@mednax.com>
2022-03-09 10:03:46 -0500

    added test to assert damage

--- test/test_character.py
+++ test/test_character.py
@@ -7,3 +7,11 @@ class TestCharacter:
         assert character.health == 1000
         assert character.level == 1
         assert character.alive
+
+# Characters can Deal Damage to Characters.
+#    - Damage is subtracted from Health
+
+    def test_character_gets_damaged(self):
+        knight = Character()        
+        dragon = Character()        
+        assert knight.damage(dragon, 2) == 1000 - 2
