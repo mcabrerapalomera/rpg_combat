56601bf1d070eb29e89818d898dea085cedbbde2
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-10 10:02:39 -0500

    Implement attack

--- rpg_combat/character.py
+++ rpg_combat/character.py
@@ -1,5 +1,11 @@
+DAMAGE = 10
+
+
 class Character:
     def __init__(self):
         self.health = 1000
         self.level = 1
         self.alive = True
+
+    def attack(self, enemy):
+        enemy.health = enemy.health - DAMAGE
--- test/test_character.py
+++ test/test_character.py
@@ -1,5 +1,6 @@
 from rpg_combat.character import Character
 
+
 class TestCharacter:
     def setup(self):
         self.knight = Character()
@@ -8,11 +9,15 @@ class TestCharacter:
         assert self.knight.health == 1000
         assert self.knight.level == 1
         assert self.knight.alive
-    
-    def test_character_can_damage_character():
+
+    # This test is failing, but not for the RIGHT reason. It should be failing on line 18 on the assertion,
+    # but it's failing on line 14. Shauna, we threw you something different here than you may be did with Chris
+    # since our tests aren't just functions, but methods within a test Class. Which means every method
+    # needs to accept a self parameter.
+    def test_character_can_damage_character(self):
         knight = Character()
         prince = Character()
         old_prince_health = prince.health
+        # OK, so I was wrong about it failing on the assertion. It fails here first because we don't have an attack method
         knight.attack(prince)
         assert prince.health < old_prince_health
-    
