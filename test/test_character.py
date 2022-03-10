b09c53dacaf1d6a640225d12d80ab3bfeca055c0
smorales <shauna_flowersmorales@mednax.com>
2022-03-10 09:55:09 -0500

    Failing test for character damaging character

--- test/test_character.py
+++ test/test_character.py
@@ -8,3 +8,11 @@ class TestCharacter:
         assert self.knight.health == 1000
         assert self.knight.level == 1
         assert self.knight.alive
+    
+    def test_character_can_damage_character():
+        knight = Character()
+        prince = Character()
+        old_prince_health = prince.health
+        knight.attack(prince)
+        assert prince.health < old_prince_health
+    
