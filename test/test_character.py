ceffc9bf4926104cebca44ad36b1ad16b5903f75
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-10 09:45:44 -0500

    Test character creation

---
+++ test/test_character.py
@@ -0,0 +1,11 @@
+from rpg_combat.character import character
+
+
+class TestCharacter:
+    def setup(self):
+        self.knight = Character()
+
+    def test_creation(self):
+        assert self.knight.health == 1000
+        assert self.knight.level == 1
+        assert self.knight.alive
